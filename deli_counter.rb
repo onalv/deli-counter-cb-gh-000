41dbbf9e8239b48251f812a1751a5892fecda2b3
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-13 15:25:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. Grace 2. Kents" + katz_deli.each_with_index{|name, i| print "#{i}. #{name} "}
+    puts "The line is currently: " + katz_deli.each_with_index{|name, i| print "#{i}. #{name} "}
   end
 end
 
